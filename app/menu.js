f5cd709bab1fe8080d385831eb6a0a0f637f3dcc
abrosimov-d <153320622+abrosimov-d@users.noreply.github.com>
2024-10-07 23:08:21 +0300

    fix

--- app/menu.js
+++ app/menu.js
@@ -1,4 +1,5 @@
 import data from "./menuData.js";
+import data2 from "./menuData2.js";
 
 export default class Menu {
     constructor(index) {
@@ -13,9 +14,14 @@ export default class Menu {
 
     getEatings() {
         let result = ''
-        let eatings = ['00:00', '00:00', '00:00', '00:00', '00:00']
+        let eatings = ['08:00 завтрак', '10:30 ланч ', '13:00 обед', '16:00 полдник', '18:00 ужин']
         for (let i = 0; i < eatings.length; i++)
-            result = result + `<div class="menu-line"><div class="time">${eatings[i]}</div> <div>${data[this.index][i].title}</div></div>`
+            result = result + `<div class="menu-line"><div class="time">${eatings[i]}</div> <div>${this.getData()[this.index][i].title}</div></div>`
         return result;
+    }
+
+    getData() {
+        console.log(data2())
+        return data;
     }
 }
